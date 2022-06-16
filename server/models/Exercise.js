1ce23f2e051f9806730ceaf373ee98d7bef2552a
Maddy <madisonschaaf48@gmail.com>
2022-06-16 17:00:43 -0600

    set up project

---
+++ server/models/Exercise.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+// const reactionSchema = require('./Reaction');
+// const dateFormat = require('../utils/dateFormat');
+
+const exerciseSchema = new Schema(
+  {
+    exerciseName: {
+      type: String,
+      required: 'You need to provide a name for your exercise',
+      minlength: 1,
+      maxlength: 280
+    },
+    weight: {
+      type: Number,
+      require: 'Please enter the weight for this exercise'
+
+    },
+    repetitions: {
+      type: Number,
+      required: 'How many reps did you do?'
+    },
+    notes: {
+      type: String
+    }
+    // ,
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    //   get: timestamp => dateFormat(timestamp)
+    // },
+    // username: {
+    //   type: String,
+    //   required: true
+    // },
+    // reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+// thoughtSchema.virtual('reactionCount').get(function() {
+//   return this.reactions.length;
+// });
+
+const Exercise = model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    # friendCount: Int
+    exercises: [Exercise]
+    # friends: [User]
+  }
+
+  type Exercise {
+    _id: ID
+    exerciseName: String
+    weight: Number
+    repetitions: Number
+    username: String
+    notes: String
+  }
+
+  # type Reaction {
+  #   _id: ID
+  #   reactionBody: String
+  #   createdAt: String
+  #   username: String
+  # }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    exercises(username: String): [Exercise]
+    exercise(_id: ID!): Exercise
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addExercise(exerciseName: String!): Exercise
+    # addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    # addFriend(friendId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
